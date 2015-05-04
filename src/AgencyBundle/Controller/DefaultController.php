c50887fdb51daaf6a518cd765afd0c7df4113e80
Juan Arias <juan.arias.freire@gmail.com>
2015-05-04 13:26:47 +0200

    Some order in the files generated, should be easier to mod now

--- src/AgencyBundle/Controller/DefaultController.php
+++ src/AgencyBundle/Controller/DefaultController.php
@@ -8,6 +8,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller {
 
     public function indexAction() {
-        return $this->render('@Agency/default/index.html.twig');
+        $availableSections = array();
+
+        $availableSections[] =
+            array("name"=>"Service", "template"=>"services.html.twig", "id"=>"services");
+        $availableSections[] =
+            array("name"=>"Portfolio", "template"=>"portfolio.html.twig", "id"=>"portfolio");
+        $availableSections[] =
+            array("name"=>"About", "template"=>"about.html.twig", "id"=>"about");
+        $availableSections[] =
+            array("name"=>"Team", "template"=>"team.html.twig", "id"=>"team");
+        $availableSections[] =
+            array("name"=>"Contact me", "template"=>"contact.html.twig", "id"=>"contact");
+
+
+        $viewData = array();
+        $viewData["sections"] = $availableSections;
+        return $this->render('@Agency/default/index.html.twig', $viewData);
     }
 }
